907d96ed4b032043017f4872230dc3e37ce188f7
Sean Askandari <seanaskandari@gmail.com>
2020-10-12 11:38:03 -0500

    added style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle `
+    body {
+      font-family: 'Open Sans', sans-serif;
+    }
+
+    h1, h2, h3 {
+      font-family: 'Alatsi', sans-serif;
+    }
+`
 
 function App() {
   return (
+  <>
+    <h1>OSTERIA</h1>
+    <GlobalStyle />
     <div>Hello Osteria</div>
+
+  </>
   );
 }
 
