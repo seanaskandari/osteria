06d82e59386acfe05dafe77291b7550df1dede84
Sean Askandari <seanaskandari@gmail.com>
2020-10-13 13:36:58 -0500

    updated favicon and title

---
+++ src/Hooks/useTitle.js
@@ -0,0 +1,11 @@
+import {useEffect} from 'react';
+
+export function useTitle({openFood, orders}) {
+    useEffect(() => {
+        if(openFood){
+            document.title = openFood.name;
+        } else {
+            document.title = orders.length === 0 ? 'Osteria App' : `[${orders.length}] items in your order!`;
+        }
+    });
+}
