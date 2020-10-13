f1bc2453034425bd4714ccf3f9f6052380441fd4
Sean Askandari <seanaskandari@gmail.com>
2020-10-12 19:02:24 -0500

    added food menu, food data, and grid

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Banner = styled.div`
 height: 200px;
-background-image: url("images/osteria-banner.jpeg");
+background-image: url("img/osteria-banner.jpeg");
 background-position: center;
 background-size: cover;
 filter: contrast(75%);
---
+++ src/Data/FoodData.js
@@ -0,0 +1,26 @@
+export const foods = [{
+    name: 'Osteria Caesar',
+    img: '/img/ostiacaesar.jpeg'
+},
+{
+    name: 'Insalata di Osteria',
+    img: '/img/ostiasalad.jpeg'
+},
+{
+    name: 'Osteria Carbonara',
+    img: '/img/ostiacarbonara.jpeg'
+},
+{
+    name: 'Osteria Tortellini',
+    img: '/img/ostiatortellini.jpeg'
+},
+{
+    name: 'Tiramisu',
+    img: '/img/ostiatiramisu.jpeg'
+},
+{
+    name: 'Macarons',
+    img: '/img/ostiamacarons.jpg'
+}
+
+];
